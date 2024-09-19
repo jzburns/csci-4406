0f66f4cc97221b7ea39af4f74c312a33c98e4962
Dr. John Burns <jzburns@users.noreply.github.com>
2024-09-19 15:01:42 +0400

    Create post-index.s
    using the post-index operator

---
+++ loops/post-index.s
@@ -0,0 +1,20 @@
+.global _start
+_start:
+	ldr r0, =array1
+	mov r1, #0
+	mov r2, #0
+	
+loop:
+	cmp r2, #6
+	bge end
+	ldr r3, [r0, r1]
+	add r3, r3, #10 
+	str r3, [r0], #4
+	add r2, r2, #1
+	b loop
+end:
+	svc #0
+	
+.data
+
+array1: .word 2,3,11,4,55,6
