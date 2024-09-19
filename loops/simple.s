aaca57a739d85c90f02518bec8a2ab1536b7f65f
Dr. John Burns <jzburns@users.noreply.github.com>
2024-09-19 15:00:36 +0400

    Update simple.s
    Simple loop

--- loops/simple.s
+++ loops/simple.s
@@ -1,17 +1,17 @@
 .global _start
 _start:
-	ldr r0, =array1 // array base pointer
-	mov r1, #0 // offset
-	mov r2, #0 // loop counter
+	ldr r0, =array1
+	mov r1, #0
+	mov r2, #0
 	
 loop:
 	cmp r2, #6
 	bge end
-	ldr r3, [r0, r1] // read base + offset to reg
-	add r3, r3, #10 // add 10 to the value
-	str r3, [r0, r1] // write reg to base + offset
-	add r2, r2, #1 // inc loop
-	lsl r1, r2, #2 // calc offset
+	ldr r3, [r0, r1]
+	add r3, r3, #10 
+	str r3, [r0, r1]
+	add r2, r2, #1 
+	lsl r1, r2, #2
 	b loop
 end:
 	svc #0
