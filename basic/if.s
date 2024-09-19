7c0286822e02663a225615079860ab815b514358
Dr. John Burns <jzburns@users.noreply.github.com>
2024-09-19 16:48:53 +0400

    Update if.s

--- basic/if.s
+++ basic/if.s
@@ -2,10 +2,17 @@
 _start:
 	mov r0, #5
 	mov r1, #8
-	cmp r0, r1
+	
+	cmp r1, r0
 	blt minus_number
+	b finalize
+
 carry_on:
-	mov r1, #8 
+	mov r1, #8
  
 minus_number:
 	mov r5, #1
+	svc 2
+	
+finalize:
+	svc 2
