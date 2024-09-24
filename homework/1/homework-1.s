fbe1a9cb6059284384b28d3599153238f89dd695
Dr. John Burns <jzburns@users.noreply.github.com>
2024-09-24 13:53:11 +0400

    Update homework-1.s

--- homework/1/homework-1.s
+++ homework/1/homework-1.s
@@ -1,4 +1,34 @@
 .global _start
 _start:
+
+step0:
+	mov r0, #2
+	mov r1, #10
 	
+step1:	
+	cmp r0, r1
+	beq step10
+	add r0, #2
 	
+step2:	
+	cmp r0, r1
+	beq step10
+	add r0, #2
+	
+step3:	
+	cmp r0, r1
+	beq step10
+	add r0, #2
+	
+step4:	
+	cmp r0, r1
+	beq step10
+	add r0, #2
+
+step5:	
+	cmp r0, r1
+	beq step10
+	add r0, #2
+	
+step10:
+	svc 2
