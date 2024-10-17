64c3c61c4b6d901ed74b4cb103bf1c277a2012ef
Dr. John Burns <jzburns@users.noreply.github.com>
2024-10-17 10:43:22 +0400

    Update homework-2.s

--- homework/2/homework-2.s
+++ homework/2/homework-2.s
@@ -1,1 +1,15 @@
+.global _start
+_start:
+	
+	bl to_upper
+	bl to_lower
 
+to_upper:
+
+to_lower:
+	
+data:
+	test_case_1: .asciz "... INTERESTING test data here: likE this @ SIGn and the % sign TOo!"
+	
+	// test_case_2 is up to you to define 
+	test_case_2: .asciz ""
